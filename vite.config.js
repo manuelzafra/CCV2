672487c3719d64985f54962dfbf8eafa1aab6090
Manuel Zafra <35781162-mz000@users.noreply.replit.com>
2024-08-28 11:36:00 +0000

    change vite

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/ccv2/', // Añade esta línea, reemplaza 'tu-repositorio' con el nombre de tu repositorio
   server: {
     host: '0.0.0.0',
   },
